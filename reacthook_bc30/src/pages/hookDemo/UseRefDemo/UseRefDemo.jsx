3590bb725e68acda275b59dd33769f0fd9f419fc
Giangducthang1208 <90580255+Giangducthang1208@users.noreply.github.com>
2022-09-01 12:45:44 +0700

    hook_buoi3

--- reacthook_bc30/src/pages/hookDemo/UseRefDemo/UseRefDemo.jsx
+++ reacthook_bc30/src/pages/hookDemo/UseRefDemo/UseRefDemo.jsx
@@ -1,27 +1,7 @@
 import React from 'react'
 
-export default function UseRefDemo(props) {
-    const handleChange = (e) => {
-
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault(); 
-    }
-
+export default function UseRefDemo() {
   return (
-    <form className='container' onSubmit={handleSubmit}>
-        <h3>Login</h3>
-        <div className='form-group'>
-            <p>username</p>
-            <input className='form-control' id='username' onInput={handleChange} />
-        </div>
-        <div className='form-group'>
-            <p>password</p>
-            <input className='form-control' id='password' type='password' onInput={handleChange}/>
-        </div>
-        <div className='form-group'>
-            <button className='btn btn-success'type='submit'> Login</button>
-        </div>
-    </form>
+    <form className='container'></form>
   )
 }
